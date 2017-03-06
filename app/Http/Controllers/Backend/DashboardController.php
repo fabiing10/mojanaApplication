8ee68c38f5886dcd54af5c19866fe53d198aeb6b
Fabian <fabiing10@gmail.com>
2017-03-06 16:27:19 -0500

    Create indicadores dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -16,4 +16,8 @@ class DashboardController extends Controller
     {
         return view('backend.dashboard');
     }
+
+    public function indicadores(){
+      return "Indicadores";
+    }
 }
---
+++ resources/views/backend/indicadores/index.blade.php
@@ -0,0 +1,50 @@
+@extends ('backend.layouts.app')
+
+@section ('title', trans('labels.backend.access.users.management'))
+
+@section('after-styles')
+    {{ Html::style("css/backend/plugin/datatables/dataTables.bootstrap.min.css") }}
+@stop
+
+@section('page-header')
+    <h1>
+        {{ trans('labels.backend.access.users.management') }}
+        <small>{{ trans('labels.backend.access.users.active') }}</small>
+    </h1>
+@endsection
+
+@section('content')
+    <div class="box box-success">
+        <div class="box-header with-border">
+            <h3 class="box-title">{{ trans('labels.backend.access.users.active') }}</h3>
+
+            <div class="box-tools pull-right">
+                @include('backend.access.includes.partials.user-header-buttons')
+            </div><!--box-tools pull-right-->
+        </div><!-- /.box-header -->
+
+        <div class="box-body">
+            <div class="table-responsive">
+                <table id="users-table" class="table table-condensed table-hover">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre Indicador</th>
+                            <th>Categoria</th>
+                            <th>Id</th>
+                            <th>Id</th>
+                            <th>Id</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div><!--table-responsive-->
+        </div><!-- /.box-body -->
+    </div><!--box-->
+
+
+@stop
+
+@section('after-scripts')
+    {{ Html::script("js/backend/plugin/datatables/jquery.dataTables.min.js") }}
+    {{ Html::script("js/backend/plugin/datatables/dataTables.bootstrap.min.js") }}
+@stop
